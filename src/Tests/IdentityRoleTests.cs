5e908a16b035dbb17593bfd47e3e85a8cb304aa6
Luis Hernandez <luis.e.hernandez.torres@gmail.com>
2019-06-10 17:27:01 -0400

    Another little break

--- src/Tests/IdentityRoleTests.cs
+++ src/Tests/IdentityRoleTests.cs
@@ -45,7 +45,7 @@
 
 			var parsed = role.Id.SafeParseObjectId();
 			Expect(parsed, Is.Not.Null);
-			Expect(parsed, Is.Not.EqualTo("noadmin"));
+			Expect(parsed, Is.Not.EqualTo("notAnAdmin"));
 		}
 	}
 }
